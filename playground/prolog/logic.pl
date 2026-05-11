09a60d04d221511908c511586a471eb3c507a1db
Mohamed-Abdalla-Yassen <mabda885@gmail.com>
2026-05-11 22:18:03 +0300

    deleting back

--- playground/prolog/logic.pl
+++
@@ -1,5 +0,0 @@
-:- include('courses_data.pl').
-
-recommend(Difficulty, _Prereq, Preference, Year, Department, Course) :-
-    course(Course, Difficulty, _, Preference, Year, Department).
-
--- playground/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('recommend/noAi/', views.get_recommendations),
-    path('recommendPost/noAi/', views.get_recommendations_post),
-    path('recommend/ai/', views.get_AI_recommendations),
-    path('recommend/complex/noAi/', views.get_recommendations_complex),
-]
